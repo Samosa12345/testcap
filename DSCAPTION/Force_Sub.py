80e164c8fba758bb498623ad3feb4fd377820daf
Samosa12345 <142589207+Samosa12345@users.noreply.github.com>
2024-08-05 16:54:52 +0530

    Update Force_Sub.py

--- DSCAPTION/Force_Sub.py
+++ DSCAPTION/Force_Sub.py
@@ -2,7 +2,6 @@
 
 # ===================== [ importing Requirements ] ===================== #
 
-
 from pyrogram import Client, filters, enums 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant

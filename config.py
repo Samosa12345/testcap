559c4e0b190abab429211f7312401309c5a77f77
Samosa12345 <142589207+Samosa12345@users.noreply.github.com>
2024-11-11 18:33:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class DS(object):
     # Bot config  ( required.. 😥)
     API_ID = os.environ.get("API_ID", "25833520")
     API_HASH = os.environ.get("API_HASH", "7d012a6cbfabc2d0436d7a09d8362af7")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7312480511:AAElQ52v_jM-PH0jQCopWixyR7o3c4fviGM")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7312480511:AAF2V4w27h5IVZM9VbC-PAouV_UpSBCQKmA")
 
     # start_pic
     START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
@@ -17,14 +17,14 @@ class DS(object):
     UPI_QR_CODE = os.environ.get("UPI_QR_CODE", "https://graph.org/file/7b782be88a32be378790f.jpg")
 
     # markdown Image
-    MARKDOWN_PIC = os.environ.get("MARKDOWN_PIC", "https://graph.org/file/007534f9b5f183ab02d01.jpg")
+    MARKDOWN_PIC = os.environ.get("MARKDOWN_PIC", "https://envs.sh/ZEt.png")
     
     # wes response configuration
     BOT_UPTIME = time.time()
     PORT = int(os.environ.get("PORT", "8080"))
 
     # force subs channel ( required.. 😥)
-    FORCE_SUB = int(os.environ.get("FORCE_SUB", -1002158291506) )
+    FORCE_SUB = int(os.environ.get("FORCE_SUB", -1002104350566) )
     
     # database config ( required.. 😥)
     DB_NAME = os.environ.get("DB_NAME", "testpubcapbot")     

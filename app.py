39154a07dff00d07f68946eb52a653483e777dfb
Samosa12345 <142589207+Samosa12345@users.noreply.github.com>
2025-05-14 04:47:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+# ===================== [ importing Requirements ] ===================== #
+
 from flask import Flask
+
+# ===================== [ Flask App Function ] ===================== #
 
 app = Flask(__name__)
 
@@ -8,3 +12,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
+# ===================== [ THE END ] ===================== #
